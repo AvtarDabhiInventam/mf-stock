626105beecd6141a322eee572e377f9351b75333
darshik <darshik.inventam@gmail.com>
2023-12-11 17:09:57 +0530

    header-footer design

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -30,32 +30,28 @@ const Header = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   return (
-    <div>
+    <div className={`${style.navbar_main_div} `}>
       <Container>
-        <Navbar
-          collapseOnSelect
-          expand="lg"
-          className={`${style.navbar_main_div} `}
-        >
+        <Navbar collapseOnSelect expand="lg">
           <Navbar.Brand
             href="#home"
             className={`${style.navbar_brand} me-0 p-0`}
           >
             <Link href="/">
               {/* <Image src={logo} width={233} height={200} alt='Banner Image' /> */}
-              <FaOpencart fontSize={50} color="#000000" />
+              <FaOpencart fontSize={50} color="#0ABB92" />
             </Link>
           </Navbar.Brand>
           <div className={`${style.mob_account_icons} `}>
-            <Link href="/account">
+            <Link href="#">
               <FaUser className={`${style.info_icon}`} />
             </Link>
-            <Link href="/cart">
+            {/* <Link href="#">
               <div className={`${style.cart_items} `}>
                 <FaShoppingCart className={`${style.info_icon}`} />
                 <span>3</span>
               </div>
-            </Link>
+            </Link> */}
           </div>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse
@@ -63,69 +59,13 @@ const Header = () => {
             className="justify-content-end"
           >
             <Nav className={`${style.navbar_nav} `}>
-              {/* <Nav.Link className={`${style.nav_link} `} href='#'>
-              Search
-            </Nav.Link> */}
-              <Nav.Link className={`${style.nav_link} `} onClick={handleShow}>
-                Login
-              </Nav.Link>
-
-              <NavDropdown
-                // className="pr-2 py-2 align-text-top"
-                title="Shop"
-                id="basic-nav-dropdown"
-                className={`${style.nav_link} ${style.mega_menu} mega-menu`}
-              >
-                <div className="pt-0 mt-0 container-fluid">
-                  <Row>
-                    <Col xs="12" md="3">
-                      <CustomNavHeader text={"TOPS"}>TOPS</CustomNavHeader>
-                      <CustomNavItem text={"Long sleeve"}></CustomNavItem>
-                      <CustomNavItem text={"3/4 sleeve"}></CustomNavItem>
-                      <CustomNavItem text={"Half sleeve"}></CustomNavItem>
-                      <CustomNavItem text={"Short sleeve"}></CustomNavItem>
-                      <CustomNavItem text={"Sleeveless"}></CustomNavItem>
-                    </Col>
-
-                    <Col xs="12" md="3">
-                      <CustomNavHeader text={"TUNICS"}>TUNICS</CustomNavHeader>
-                      <CustomNavItem text={"Long sleeve"}></CustomNavItem>
-                      <CustomNavItem text={"3/4 sleeve"}></CustomNavItem>
-                      <CustomNavItem text={"Short sleeve"}></CustomNavItem>
-                    </Col>
-
-                    <Col xs="12" md="3">
-                      <CustomNavHeader text={"DRESSES"}>
-                        DRESSES
-                      </CustomNavHeader>
-                      <CustomNavItem text={"Long sleeve"}></CustomNavItem>
-                      <CustomNavItem text={"3/4 sleeve"}></CustomNavItem>
-                      <CustomNavItem text={"Short sleeve"}></CustomNavItem>
-                      <CustomNavItem text={"Maxi dresses"}></CustomNavItem>
-                    </Col>
-                    <Col xs="12" md="3">
-                      <CustomNavHeader text={"DRESSES"}>
-                        ACCESSORIES
-                      </CustomNavHeader>
-                      <CustomNavItem text={"View all"}></CustomNavItem>
-                    </Col>
-                  </Row>
-                </div>
-              </NavDropdown>
-              <Nav.Link
-                className={`${style.nav_link} menu-mob-hide`}
-                href="/account"
-              >
-                <FaUser className={`${style.info_icon}`} />
-              </Nav.Link>
-              <Nav.Link
-                className={`${style.nav_link} menu-mob-hide`}
-                href="/cart"
-              >
-                <div className={`${style.cart_items} `}>
-                  <FaShoppingCart className={`${style.info_icon}`} />
-                  <span>3</span>
-                </div>
+              <Nav.Link className={`${style.nav_link} `}> Signup</Nav.Link>
+              <Nav.Link className={`${style.nav_link} `}> About</Nav.Link>
+              <Nav.Link className={`${style.nav_link} `}> Pricing</Nav.Link>
+              <Nav.Link className={`${style.nav_link} `}> Support</Nav.Link>
+              <Nav.Link className={`${style.nav_link} `}>
+                <FaUser className={`${style.info_icon} me-3`} />
+                Account
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
