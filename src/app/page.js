cfd3d48e5a8b326da2b804f7fd8490722ab00ed4
AvtarDabhiInventam <108725375+AvtarDabhiInventam@users.noreply.github.com>
2023-12-11 17:20:54 +0530

    homepage changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,72 @@
+"use client";
 import Image from "next/image";
 import styles from "../styles/page.module.scss";
-import { Button } from "react-bootstrap";
-
+import { Button, Card, Col, Row, Tab, Tabs } from "react-bootstrap";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="container">
-      <h1 className="text-muted">Homepage</h1>
-      <Button variant="primary">fdsf</Button>
+      <Tabs
+        defaultActiveKey="Stock"
+        transition={false}
+        id="noanim-tab-example"
+        className="mb-3"
+      >
+        <Tab eventKey="Stock" title="Stock">
+          <Row>
+          <Col xs={12} md={12}>
+            <div className="d-flex justify-content-between align-items-center">
+                <div><h2 className="section-title">Index</h2></div>
+                <div><Link href="/">All indices</Link></div>
+            </div>
+          </Col>
+            <Col xs={12} md={3}>
+              <Card className={`${styles.nifty_index}`}>
+                <Card.Body>
+                  <Card.Title>NIFTY 50</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    20,997
+                  </Card.Subtitle>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs={12} md={3}>
+              <Card className={`${styles.nifty_index}`}>
+                <Card.Body>
+                  <Card.Title>BANKNIFTY</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    20,997
+                  </Card.Subtitle>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs={12} md={3}>
+              <Card className={`${styles.nifty_index}`}>
+                <Card.Body>
+                  <Card.Title>SENSEX</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    20,997
+                  </Card.Subtitle>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs={12} md={3}>
+              <Card className={`${styles.nifty_index}`}>
+                <Card.Body>
+                  <Card.Title>FINNIFTY</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    20,997
+                  </Card.Subtitle>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Tab>
+        <Tab eventKey="MutualFunds" title="MutualFunds">
+          Tab content for Mutual Funds
+        </Tab>
+      </Tabs>
     </main>
   );
 }
