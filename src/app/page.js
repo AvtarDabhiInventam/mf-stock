8c5171d0a367a7285b223ac31a3f36baf5ac82a2
AvtarDabhiInventam <108725375+AvtarDabhiInventam@users.noreply.github.com>
2023-12-11 15:18:45 +0530

    css variable

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "../styles/page.module.scss";
+import { Button } from "react-bootstrap";
+
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Homepage</h1>
+    <main className="container">
+      <h1 className="text-muted">Homepage</h1>
+      <Button variant="primary">fdsf</Button>
     </main>
   );
 }
