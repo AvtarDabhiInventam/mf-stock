1e96f5858ad0bba71c81989c6f14c53238010152
AvtarDabhiInventam <108725375+AvtarDabhiInventam@users.noreply.github.com>
2023-12-21 16:23:34 +0530

    design improvement

--- src/component/index-market/index.js
+++ src/component/index-market/index.js
@@ -2,35 +2,46 @@ import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
 import styles from "../../styles/stock-card.module.scss";
 import { MAIN_CARD } from "@/jsondata/stockConstant";
+import Slider from "react-slick";
 
 console.log("NiftyIndex", MAIN_CARD);
 
 const IndexMarket = () => {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    centerPadding: "60px",
+    className: "center",
+  };
   return (
     <>
-      <Row className="row_space">
+      <Slider {...settings}>
         {MAIN_CARD.map((value, index) => {
           return (
             <>
-              <Col xs={6} md={4}>
-                <Card className={`${styles.card_main_div}`}>
-                  <Card.Body>
-                    <h6 className={`${styles.title}`}>{value.name}</h6>
-                    <p className={`${styles.index_value}`}>
-                      {value.price}{" "}
-                      <label
-                        className={`${styles.index_percentage} green-color`}
-                      >
-                        {value.priceUpDown} ({value.priceUpDownPercentage}%)
-                      </label>
-                    </p>
-                  </Card.Body>
-                </Card>
-              </Col>
+              <Card className={`${styles.card_main_div}`}>
+                <Card.Body>
+                  <h6 className={`${styles.title}`}>{value.name}</h6>
+                  <p className={`${styles.index_value}`}>
+                    {value.price}{" "}
+                    <label
+                      className={`${styles.index_percentage} ${
+                        value.priceUp ? "green-color" : "text-danger"
+                      } `}
+                    >
+                      {value.priceUp ? value.priceUp : value.priceDown} (
+                      {value.pricePercentage}%)
+                    </label>
+                  </p>
+                </Card.Body>
+              </Card>
             </>
           );
         })}
-      </Row>
+      </Slider>
     </>
   );
 };
